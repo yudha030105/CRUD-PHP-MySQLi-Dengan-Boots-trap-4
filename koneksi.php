c71f4eef7f61854d633b73482bd56c852e9ff209
radinallnr <radinallsnr@gmail.com>
2023-01-09 13:56:30 +0700

    rename

--- koneksi.php
+++ koneksi.php
@@ -14,20 +14,4 @@ if($connection) {
     echo "Koneksi Gagal! : ". mysqli_connect_error();
 }
 
-?><?php
-
-//deklasrasi variabel
-$db_host = "localhost"
-$db_user = "root";
-$db_pass = "";
-$db_name = "db_sekolah";    
-
-$connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
-if($connection) {
-    echo "Koneksi Berhasil!";
-} else {
-    echo "Koneksi Gagal! : ". mysqli_connect_error();
-}
-
 ?>
